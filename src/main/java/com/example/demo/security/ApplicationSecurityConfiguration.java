4b4dff139a988ec5f8fdfac47a1fa1d652d456b0
unknown <imad.razok.22@gmail.com>
2020-10-24 21:30:18 +0100

    in memory user

--- src/main/java/com/example/demo/security/ApplicationSecurityConfiguration.java
+++ src/main/java/com/example/demo/security/ApplicationSecurityConfiguration.java
@@ -1,15 +1,27 @@
 package com.example.demo.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableWebSecurity
 public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapter {
+    private final PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ApplicationSecurityConfiguration(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -25,6 +37,13 @@ public class ApplicationSecurityConfiguration extends WebSecurityConfigurerAdapt
     @Override
     @Bean
     protected UserDetailsService userDetailsService() {
-        return super.userDetailsService();
+        UserDetails userDetails = User.builder().
+                username("issamShimi")
+                .password(passwordEncoder.encode("pass"))
+                .roles("admin") //Role admin
+                .build();
+        return new InMemoryUserDetailsManager(
+            userDetails
+        );
     }
 }
